8d94d1fe870b07c508c3e1777545d613f196e3f4
tripura <140330781+tripura28@users.noreply.github.com>
2026-04-09 16:40:05 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,32 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #datatypes
+>>> a=5
+>>> type(a)
+<class 'int'>
+>>> b=1.2
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d="codegnan"
+>>> type(d)
+<class 'str'>
+>>> e='''Tripura'''
+>>> print(e)
+Tripura
+>>> f=2+4j
+>>> type(f)
+<class 'complex'>
+>>> g=4+6i
+SyntaxError: invalid decimal literal
+>>> x=True
+>>> type(x)
+<class 'bool'>
+>>> y="true"
+>>> type(y)
+<class 'str'>
+>>> g=4j
+>>> type(g)
+<class 'complex'>
