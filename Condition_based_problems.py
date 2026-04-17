ff232b803eed82b8c4a9af22a4914135103b47ec
tripura <140330781+tripura28@users.noreply.github.com>
2026-04-17 17:59:05 +0530

    Add files via upload

---
+++ Condition_based_problems.py
@@ -0,0 +1,165 @@
+#conditions
+#if condition by using comparision operators
+#<,>,<=,>=,==,!=
+
+'''a=2
+b=4
+if a<b:
+    print("it is less")'''
+
+'''a=2
+b=4
+if a>b:
+    print("it is less")'''
+
+
+'''a=5
+b=10
+if a<=b:
+    print("true")'''
+
+
+'''a=7
+b=11
+if b>=a:
+    print("greater")'''
+
+
+'''a=2
+b=4
+if a!=b:
+    print("it is less")'''
+
+
+'''a=10
+b=10
+if a==b:
+    print("it is equal")'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+
+'''a=int(input("a value"))
+if a<15:
+    print("less")'''
+
+'''a="python"
+if a!="java":
+    print("true")'''
+
+'''a="python"
+if a=="python":
+    print("true)'''
+
+'''a=input("name")
+if a=="Tripura":
+    print("true")'''
+    
+
+#if condition by using logical operators
+#and, or, not
+
+'''a=9
+b=12
+if a<b and b>a:
+    print("true")'''
+
+'''a=9
+b=12
+if a<=b and b>=a:
+    print("true")'''
+
+
+'''a=20
+b=30
+if a!=b and b==a:
+    print("true")'''
+
+
+'''a=4
+b=8
+if a<b or b>a:
+    print("true")'''
+
+
+'''a=5
+b=10
+if a<=b or b>=a:
+    print("true")'''
+
+
+'''a=9
+b=12
+if a!=b or b==a:
+    print("true")'''
+
+
+'''a=6
+b=9
+if not a<b:
+    print("it is less")'''
+
+
+'''a=6
+b=9
+if not a>b:
+    print("it is less")'''
+    
+
+'''a=int(input("enter age:"))
+if a>=18:
+    print("major, you can vote!")'''
+
+#identity operator
+#is,isnot
+
+'''a=8
+if type(a) is int:
+    print("it is int")'''
+
+
+'''a=8
+if type(a) is not int:
+    print("it is not int")'''
+
+'''a=int(input("enter the value"))
+if type(a) is int:
+    print("true")'''
+
+    
+'''a=3.4
+if type(a) is float:
+    print("float")'''
+
+
+'''a=input("enter value:")
+if type(a) is float:
+    print("it is a float")'''
+
+#membership operator
+#in, not in
+
+'''a=[2,3,4,5,5]
+if 7 in a:
+    print("true")'''
+
+
+'''a=[3,4,5,5,2,6]
+if 5 not in a:
+    print("true")'''
+
+
+'''a=[3,4,5,6,2]
+if 10 not in a:
+    print("true")'''
+
+
+'''a=[3,4,5,6,7,3,8,9]
+b=int(input("enter a value:"))
+if b in a:
+    print("true")'''
+
---
+++ basic_problems.py
@@ -0,0 +1,104 @@
+'''a=4
+b=8
+print(a+b)'''
+
+#print(3+4)
+
+#run_time input()
+'''a=int(input("a value"))
+b=int(input("b value"))
+print(a+b)'''
+
+'''a=float(input("a value"))
+b=float(input("b value"))
+print(a+b)'''
+
+'''a=str(input("a value"))
+b=str(input("b value"))
+print(a+b)'''
+
+'''a=input("a value")
+b=input("b value")
+print(a+b)'''
+
+'''fname=input("fname")
+lname=input("lname")
+print((fname+" "+lname).title())'''
+
+'''city=input("enter the city")
+print("city is",city)'''
+
+'''a=complex(input("a value"))
+b=complex(input("b value"))
+print(a+b)'''
+
+'''a=bool(input("a value"))
+b=bool(input("b value"))
+c=bool(input("c value"))
+print(a+b+c)''' 
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(choose the option
+                        1.add
+                        2.sub
+                        3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a=input()
+print(a)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+a,b=b,a
+print(a,b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+c=a
+a=b
+b=c
+print(a,b)'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+a=a+b
+b=a-b
+a=a-b
+print(a,b)'''
+
+a=int(input("a value"))
+b=int(input("b value"))
+print("Value of a is %d" %b)
+print("value of b is %d" %a)
+
+
+
+
+
+
+
+
